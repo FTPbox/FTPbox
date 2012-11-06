22f6741e582b846aaa08c8d3e0151ab98ab0ffde
Ioannis G. <jtgftw@gmail.com>
2012-11-06 13:45:05 +0000

    Removed the WebUI
    the webUI can now be found in its own repo:
    https://github.com/FTPbox/Web-Interface

--- Web Interface/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('system/classes/Filesystem.php');
-require_once('system/classes/Gui.php');
-require_once('system/classes/Security.php');
-$security = new Security;
-$gui = new Gui;
-$filesystem = new Filesystem;
-echo $gui->renderResults($filesystem->read($_GET['absolutePath'], $_GET['relativePath'], $_GET['sortName'], $_GET['sortOrder']));
-?>
